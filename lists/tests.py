7033cee9e1d20e704e45d978ee6838f80543f907
Dong-ryull Shin / Ryan <1982sdr@hanmail.net>
2019-12-08 15:14:06 +0900

    new URL, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -41,7 +41,7 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
     def test_saving_and_retrieving_items(self):
         first_item = Item()
@@ -65,13 +65,16 @@ class HomePageTest(TestCase):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
 
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
     # Rendering Items in the Template
     def test_displays_all_list_items(self):
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
 
-        response = self.client.get('/')
+        response = self.client.get('/lists/the-only-list-in-the-world/')
 
-        self.assertIn('itemey 1', response.content.decode('utf8'))
-        self.assertIn('itemey 2', response.content.decode('utf8'))
-
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
--- lists/views.py
+++ lists/views.py
@@ -8,7 +8,9 @@ def home_page(request):
         # .objects.create is a neat shorthand for creating a new Item, without needing to call .save().
         Item.objects.create(text = request.POST['item_text'])
         # Redirect After a POST
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
+    return render(request, 'home.html')
 
+def view_list(request):
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
